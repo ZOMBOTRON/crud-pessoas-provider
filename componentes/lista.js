23487c5cca958f967f5b5269d93ad96139ee0189
Welton Júnior <weltonjr@rede.ulbra.br>
2024-03-30 17:15:17 -0300

    filtroPronto

--- componentes/lista.js
+++ componentes/lista.js
@@ -1,6 +1,10 @@
-import { View,
+import React, { useState } from 'react';
+import {
+  View,
   StyleSheet,
-  FlatList} from 'react-native';
+  FlatList,
+  TextInput,
+} from 'react-native';
 import {
   List,
   Text,
@@ -11,16 +15,8 @@ import {
 } from 'react-native-paper';
 import { useAppContext } from './provider';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import { useState } from 'react';
 import Editar from './editar';
 
-/**
- * Este componente apresenta a lista de pessoas cadastradas.
- *
- * Cada item da lista, ao ser selecionado, apresenta um retorno
- * visual, para indicar que o item está selecionado, e
- * um botão que permite excluir o item da lista de pessoas.
- */
 export default function Lista() {
   const {
     pessoas,
@@ -30,78 +26,67 @@ export default function Lista() {
   } = useAppContext();
 
   const [modalVisible, setModalVisible] = useState(false);
-  const [nome , setNome] = useState('');
+  const [nome, setNome] = useState('');
+  const [searchText, setSearchText] = useState('');
 
   const { colors, isV3 } = useTheme();
   const safeArea = useSafeAreaInsets();
 
-
-  /**
-   * Esta função é utilizada para renderizar um item da lista.
-   * Se o item da lista estiver selecionado, então adota
-   * uma cor de fundo diferente. Além disso, se o item
-   * estiver selecionado, apresenta um botão que permite
-   * excluir da lista de pessoas.
-   */
   const renderItem = ({ item }) => {
-    const selecionado = item.id == pessoaSelecionada?.id;
+    const selecionado = item.id === pessoaSelecionada?.id;
 
-
-    const Botoes = () => {
-
-      return (
-        <>
-          <IconButton
-            key={item.id}
-            icon="account-edit-outline"
-            mode="contained"
-            onPress={() => {setModalVisible(true), setNome(item.nome)}}
-          />
-          <IconButton
-            icon="trash-can-outline"
-            mode="contained"
-            onPress={() => removerPessoa(pessoaSelecionada)}
-          />
-        </>
-      );
-    };
+    const Botoes = () => (
+      <>
+        <IconButton
+          key={item.id}
+          icon="account-edit-outline"
+          mode="contained"
+          onPress={() => {
+            setModalVisible(true);
+            setNome(item.nome);
+          }}
+        />
+        <IconButton
+          icon="trash-can-outline"
+          mode="contained"
+          onPress={() => removerPessoa(pessoaSelecionada)}
+        />
+      </>
+    );
 
     return (
       <>
-      <List.Item
-        title={item.nome}
-        style={selecionado && styles.item_selecionado}
-        onPress={() => selecionarPessoa(item)}
-        left={() => (
-          <View style={styles.avatar}>
-            <Avatar.Text size={40} label={item.iniciais} />
-          </View>
-        )}
-        right={selecionado && Botoes}
-      ></List.Item>
+        <List.Item
+          title={item.nome}
+          style={selecionado && styles.item_selecionado}
+          onPress={() => selecionarPessoa(item)}
+          left={() => (
+            <View style={styles.avatar}>
+              <Avatar.Text size={40} label={item.iniciais} />
+            </View>
+          )}
+          right={selecionado && Botoes}
+        />
       </>
     );
   };
 
+  const filteredPessoas = pessoas.filter(
+    pessoa => pessoa.nome.toLowerCase().includes(searchText.toLowerCase())
+  );
+
   return (
     <View style={styles.container}>
-      <List.Section>
-        <List.Subheader>
-          <View style={styles.cabecalho}>
-            <Text style={styles.cabecalho_titulo} variant="bodyLarge">
-              Pessoas cadastradas
-            </Text>
-            {pessoas?.length > 0 && (
-              <Text variant="bodySmall">
-                Pressione um item da lista para selecionar e outra vez para
-                remover a seleção
-              </Text>
-            )}
-          </View>
-        </List.Subheader>
-      </List.Section>
+      {pessoas?.length > 0 && (
+        <TextInput
+          style={styles.searchInput}
+          placeholder="Pesquisar por nome"
+          onChangeText={setSearchText}
+          value={searchText}
+        />
+      )}
       <FlatList
-        data={pessoas}
+        data={filteredPessoas}
         renderItem={renderItem}
         keyExtractor={(item) => item.id}
         ItemSeparatorComponent={Divider}
@@ -112,10 +97,11 @@ export default function Lista() {
         )}
       />
       <Editar
-      modalVisible={modalVisible}
-      setModalVisible={setModalVisible}
-      nome={nome}
-      setNome={setNome} />
+        modalVisible={modalVisible}
+        setModalVisible={setModalVisible}
+        nome={nome}
+        setNome={setNome}
+      />
     </View>
   );
 }
@@ -136,5 +122,12 @@ const styles = StyleSheet.create({
   avatar: {
     flexDirection: 'row',
     alignItems: 'center',
-  }
+  },
+  searchInput: {
+    padding: 10,
+    marginBottom: 10,
+    borderWidth: 1,
+    borderColor: 'gray',
+    borderRadius: 5,
+  },
 });
