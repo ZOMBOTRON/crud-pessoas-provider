37abdda9747c5b42e8482374052da09e52333a3f
felipe noobrega <felipenobregacp@gmail.com>
2024-03-30 16:51:37 -0300

    fix (help): corrige modal help para ficar de acordo com outros elementos de estilos do projeto

--- componentes/help.js
+++ componentes/help.js
@@ -1,32 +1,32 @@
 
-import { Modal,  Text} from 'react-native-paper';
-import { StyleSheet, SafeAreaView } from 'react-native';
+import { Text } from 'react-native-paper';
+import { StyleSheet, SafeAreaView, Modal, TouchableOpacity } from 'react-native';
 import { View } from 'react-native-web';
 
 
-export default function help({ visible, }) {
+export default function help({ visible, setVisible }) {
     if (visible) {
         return (
-            <Modal 
+            <Modal
                 style={styles.modal}
-                visible ={visible}
-                 >
+                visible={visible}
+            >
                 <View>
-                <Text style={styles.text} variant="headlineLarge">
-                    Integrantes:
-                </Text>
-                <Text style={styles.text}>
-                    - Victoria Locatelli
-                </Text>
-                <Text style={styles.text}>
-                    - Welton Moreira Borges Júnior
-                </Text>
-                <Text style={styles.text}>
-                    - Felipe Nóbrega
-                </Text>
-                <Text style={styles.text}>
-                    - Kauan
-                </Text>
+                    <Text style={styles.text} variant="headlineLarge">
+                        Integrantes:
+                    </Text>
+                    <Text style={styles.text}>
+                        - Victoria Locatelli
+                    </Text>
+                    <Text style={styles.text}>
+                        - Welton Moreira Borges Júnior
+                    </Text>
+                    <Text style={styles.text}>
+                        - Felipe Nóbrega
+                    </Text>
+                    <Text style={styles.text}>
+                        - Kauan
+                    </Text>
                 </View>
                 <Text style={styles.text} variant="headlineLarge">
                     Propósito do App:
@@ -36,7 +36,7 @@ export default function help({ visible, }) {
                     amigável para que os usuários possam armazenar e acessar
                     facilmente os detalhes de suas conexões pessoais.
                 </Text>
-                <Text style={styles.text} variant="headlineLarge"> 
+                <Text style={styles.text} variant="headlineLarge">
                     Benefícios:
                 </Text>
                 <Text style={styles.text}>
@@ -45,6 +45,11 @@ export default function help({ visible, }) {
                     Personalização: Oferece recursos como filtragem e edição para adaptar as informações de contato às necessidades individuais dos usuários.
                     Transparência: A seção de informações sobre o aplicativo promove a transparência e confiança dos usuários ao fornecer detalhes relevantes sobre o aplicativo e seus desenvolvedores.
                 </Text>
+                <View style={styles.modalButton}>
+                    <TouchableOpacity style={styles.button} onPress={() => { setVisible(!visible) }}>
+                        <Text style={styles.textStyle}>Press Here</Text>
+                    </TouchableOpacity>
+                </View>
 
             </Modal>
         )
@@ -55,15 +60,33 @@ export default function help({ visible, }) {
 }
 const styles = StyleSheet.create({
     modal: {
-        paddingTop:30,
-        flex: 1,
-        backgroundColor:"#343a40"
+        paddingTop: 30,
+        backgroundColor: '#343a40',
+        opacity: 0.4,
     },
-    text: {    
-        color:"#fff",
+    text: {
+        color: "#000",
         margin: 10,
         textAlign: "left",
-        textAlignVertical: "top",  
+        textAlignVertical: "top",
+    },
+    textStyle: {
+        color: 'white',
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+    button: {
+        borderRadius: 20,
+        backgroundColor: '#2196F3',
+        padding: 10,
+        elevation: 2
+    },
+    modalButton: {
+        display: 'flex',
+        alignItems: "center",
+        alignContent: 'center',
+        marginTop: 20
+
     }
-  
+
 });
